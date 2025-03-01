dbc85565900f4d14307f240024aed9534bc201ed
fabiha23 <fabiha.amatullah@gmail.com>
2025-03-01 22:28:04 +0600

    task js complete

--- script/task.js
+++ script/task.js
@@ -3,9 +3,29 @@ const complete = document.getElementsByClassName('complete');
 for (let button of complete) {
     button.addEventListener('click', function() {
         alert("Board Updated Successfully");
-        console.log('ok')
-      
+        
+        const convertedNumber = textConvertedToNumber("tasks");
+        
+        let subs=convertedNumber-1;
+        document.getElementById("tasks").innerText=subs;
 
+        const convertedNumber2 = textConvertedToNumber("nav-task");
+        let sum=convertedNumber2+1;
+        document.getElementById("nav-task").innerText=sum;
+
+        let parentCard = button.parentElement.parentElement; 
+        let taskTitle = parentCard.querySelector("h2").innerText;
+
+        const p =document.createElement("p");
+        p.classList.add("bg-[#F4F7FF]","mt-4","mx-3", "p-2", "rounded-md","font-medium", "font-poppins");
+        p.innerText=` You have completed the task ${taskTitle} at ${new Date().toLocaleTimeString()}`;
+        document.getElementById("activity").appendChild(p);
+
+        button.disabled=true;
+        button.classList.add("opacity-50"); 
+        button.classList.remove("cursor-pointer");
 
     });
   }
+
+  
---
+++ utilities/utilities.js
@@ -0,0 +1,5 @@
+function textConvertedToNumber(id){
+    const text =document.getElementById(id).innerText;
+    const convertedIntoNumber = parseInt(text);
+    return convertedIntoNumber;
+}
