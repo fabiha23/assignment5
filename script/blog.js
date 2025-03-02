9c55a07da584e6493af20a6f3cbac2cf75379eb7
fabiha23 <fabiha.amatullah@gmail.com>
2025-03-02 19:06:26 +0600

    done

---
+++ script/blog.js
@@ -0,0 +1,5 @@
+const back = document.getElementById("back");
+back.addEventListener("click",function(){
+    console.log("hi")
+    window.location.href="index.html";
+})
--- script/other.js
+++ script/other.js
@@ -1,8 +1,28 @@
+function color(){
+    let v1=Math.floor(Math.random() * 256);
+    let v2=Math.floor(Math.random() * 256);
+    let v3=Math.floor(Math.random() * 256);
 
-const back = document.getElementById("back");
-back.addEventListener("click",function(){
-    console.log("hi")
-    window.location.href="index.html";
+    return `rgba(${v1}, ${v2}, ${v3}, .5)`;
+}
+
+const discover = document.getElementById("discover");
+console.log(discover);
+discover.addEventListener("click",function(){
+    window.location.href="blog.html"
+})
+
+const themeBtn = document.getElementById("theme-btn");
+themeBtn.addEventListener("click", function(){
+    const body = document.body;
+    let newColor = color();
+    body.style.backgroundColor=newColor;
+
+})
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click",function(){
+    document.getElementById("new").innerText="";
 })
 
 
